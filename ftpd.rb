59060e8cb8c260fa51b2a7e223f185ce40ec0999
Pablo Merino <pablo.perso1995@gmail.com>
2012-05-22 19:15:11 +0200

    modified copyright on files

--- ftpd.rb
+++ ftpd.rb
@@ -2,7 +2,7 @@
 ## Manzana-FTPd
 ## A featured FTP server written in Ruby
 #
-# version: 3 (2006-03-09)
+# version: 4 (2012-05-22)
 #
 # Original author:  chris wanstrath // chris@ozmm.org
 # New author:  pablo merino // pablo.perso1995@gmail.com
@@ -10,7 +10,7 @@
 # New site:    http://github.com/pablo-merino/manzana-ftpd
 #
 # license: MIT License // http://www.opensource.org/licenses/mit-license.php
-# copyright: (c) two thousand six chris wanstrath
+# copyright: (c) two thousand six chris wanstrath | two thousand twelve pablo merino
 #
 # tested on: ruby 1.8.4 (2005-12-24) [powerpc-darwin8.4.0] | ruby 1.9.3-p125 (2012-05-22) [intel osx 10.7.3]
 #
